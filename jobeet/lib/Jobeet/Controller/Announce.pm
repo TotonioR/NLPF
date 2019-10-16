b66a7ce41b07b01fff141228eb5433b7b2080964
Benjamin Birouste <benjamin.birouste@epita.fr>
2019-10-16 16:18:42 +0200

    [Annonce] fixing announce display

--- jobeet/lib/Jobeet/Controller/Announce.pm
+++ jobeet/lib/Jobeet/Controller/Announce.pm
@@ -39,7 +39,6 @@ sub list {
     my $user = $self->db->resultset('User')->search({id => $id})->first;
  	my $profile = $self->db->resultset('ProfileCandidat')->search({user_id => $id})->first;
 	my $is_candidat = defined $profile;
-	print "Content: $is_candidat\n";
 	my $announces = $self->db->resultset('Announce')->search({});
 	$self->render(announces => $announces, status => $id, is_candidat => $is_candidat, tags => $self->db->resultset('TagAnnounce'));
 }
